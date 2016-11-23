dd29c96d9d92f97c4d392619b639f88144c85661
gwaygenomics <gregory.way@gmail.com>
2016-11-23 10:10:20 -0500

    update process rnaseq script
    update absolute file paths and naming conventions

--- scripts/process_rnaseq.py
+++ scripts/process_rnaseq.py
@@ -6,25 +6,22 @@ scripts/process_rnaseq.py
 Cleans up the downloaded RNAseq data and zero-one normalizes
 
 Usage:
-Run only once by ANALYSIS.sh
+Run only once by run_pipeline.sh
 
 Output:
 Normalized PCL file with genes as rows and sample IDs as columns and MAD genes
 if option is selected
 """
 
+import os
 import pandas as pd
 from sklearn import preprocessing
 from statsmodels.robust import scale
 import argparse
 
-####################################
-# Define Functions
-####################################
 
-
-def normalize_data(data, out_fh, mad=False,
-                   mad_fh='tables/full_mad_genes.tsv',
+def normalize_data(data, out_file, mad=False,
+                   mad_fh=os.path.join('tables', 'full_mad_genes.tsv'),
                    output=True,
                    method='minmax'):
     """
@@ -32,7 +29,7 @@ def normalize_data(data, out_fh, mad=False,
 
     Arguments:
     :param data: pandas DataFrame genes as rows and sample IDs as columns
-    :param out_fh: the file name to write normalized matrix
+    :param out_file: the file name to write normalized matrix
     :param mad: boolean indicating if MAD genes should be output to file
     :param mad_fh: the file name to write mad genes
     :param method: the type of scaling to perform (defaults to minmax)
@@ -60,7 +57,7 @@ def normalize_data(data, out_fh, mad=False,
                                   columns=data.index).T
     # Write to file
     if output:
-        data_normalize.to_csv(out_fh, sep='\t', header=True, index=True)
+        data_normalize.to_csv(out_file, sep='\t', header=True, index=True)
     else:
         return data_normalize
 
@@ -70,15 +67,11 @@ def normalize_data(data, out_fh, mad=False,
         all_mad_genes = pd.Series(all_mad_genes,
                                   index=data_normalize.index.values)
         all_mad_genes = all_mad_genes.sort_values(ascending=False)
-
         all_mad_genes.to_csv(mad_fh, sep='\t', header=False)
 
 
 if __name__ == '__main__':
 
-    ####################################
-    # Load Command Arguments
-    ####################################
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--tissue", dest="tissues",
                         help="tissues to normalize")
@@ -86,40 +79,38 @@ if __name__ == '__main__':
                         help="boolean to output mad genes", default=True)
     parser.add_argument("-c", "--concatenate", dest="concatenate",
                         help="boolean to normalize together", default=False)
-    parser.add_argument("-s", "--scale", dest="scale",
+    parser.add_argument("-s", "--scale", dest="scale_x",
                         help="method to scale x matrix", default='minmax')
     args = parser.parse_args()
 
-    ####################################
-    # Load Constants
-    ####################################
-    TISSUES = args.tissues.split(',')
-    CONCATENATE = args.concatenate
-    MAD = args.mad
-    SCALE = args.scale
+    tissues = args.tissues.split(',')
+    concatenate = args.concatenate
+    mad = args.mad
+    scale_x = args.scale_x
 
-    if CONCATENATE == 'all':
-        PANCAN_FH = args.tissues.replace(',', '-')
-    elif CONCATENATE:
-        PANCAN_FH = CONCATENATE.replace(',', '-')
+    if concatenate == 'all':
+        pancan_file = args.tissues.replace(',', '-')
+    elif concatenate:
+        pancan_file = concatenate.replace(',', '-')
 
-    #########################
     # Load Data
-    #########################
     tissue_list = []
-    for tissue in TISSUES:
-        raw_fh = 'data/X/raw/' + tissue + '_RNAseq_X_matrix.tsv'
-        out_fh = 'data/X/normalized/' + tissue + '_X_normalized.tsv'
-        raw = pd.read_csv(raw_fh, delimiter='\t', index_col=0)
-        if CONCATENATE:
-            if CONCATENATE == 'all':
-                tissue_list.append(raw)
-            elif tissue in CONCATENATE.split(','):
-                tissue_list.append(raw)
+    for tissue in tissues:
+        raw_file = os.path.join('data', 'X', 'raw',
+                                '{}_RNAseq_X_matrix.tsv'.format(tissue))
+        out_file = os.path.join('data', 'X', 'normalized',
+                                '{}_X_normalized.tsv'.format(tissue))
+        raw_df = pd.read_csv(raw_file, delimiter='\t', index_col=0)
+        if concatenate:
+            if concatenate == 'all':
+                tissue_list.append(raw_df)
+            elif tissue in concatenate.split(','):
+                tissue_list.append(raw_df)
         else:
-            normalize_data(raw, out_fh, MAD, method=SCALE)
+            normalize_data(raw_df, out_file, mad, method=scale_x)
 
-    if CONCATENATE:
-        PANCAN_FH = 'data/X/normalized/' + PANCAN_FH + '_X_normalized.tsv'
-        raw = pd.concat(tissue_list, axis=1)
-        normalize_data(raw, PANCAN_FH)
+    if concatenate:
+        pancan_file = os.path.join('data', 'X', 'normalized',
+                                   '{}_X_normalized.tsv'.format(pancan_file))
+        raw_df = pd.concat(tissue_list, axis=1)
+        normalize_data(raw_df, pancan_file)
