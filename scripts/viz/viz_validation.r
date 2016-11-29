4f15a4a29a72d31d1ba28786027bfe6965e791e7
Greg Way <gregory.way@gmail.com>
2016-11-29 12:19:16 -0500

    Update validation (#14)
    * ignore validation files
    
    * update validation script
    
    mostly file name and code style updates
    
    * update validation visualization and power analysis
    
    updating code style, improved plotting and saving plots, add effect size and power analysis with plots
    
    * update validation
    
    arrange in grid spec, enlarge text, reduce dimension of power plot
    
    * remove figure file paths
    
    no longer saving these figures separately

--- scripts/viz/viz_validation.r
+++ scripts/viz/viz_validation.r
@@ -1,34 +1,40 @@
 # Gregory Way 2016
 # NF1 Classifier GBM/LGG
-# scripts/viz/viz_validation.r
+# scripts/viz/viz_validation.R
 #
 # vizualize the validation results
 #
 # Usage:
-# Run by ANALYSIS.sh
+# Run by run_pipeline.sh
 #
 # Output:
-# Three validation set figures
+# Four validation set figures and one table
 # 1) Boxplot of ensemble predictions
 # 2) Scatterplot of predictions by quantified protein
 # 3) Boxplot of predictions for NF1 WT/Inactive protein
+# 4) Power analysis plot of n x power
+# 5) Results of t-test/effect size analysis
 
 # Load libraries
 library(ggplot2)
+library(gridExtra)
 library(dplyr)
 library(calibrate)
+library(pwr)
+library(lsr)
 
 # Load Command Args
 args <- commandArgs(trailingOnly = T)
 print(args)
 validation_fh <- args[1]
 protein_fh <- args[2]
-figure_box <- paste0(args[3], 'prediction_box.png')
-figure_scatter <- paste0(args[3], 'protein_scatter.png')
-figure_pred <- paste0(args[3], 'protein_box.png')
+figure_main <- paste0(args[3], '_main_figure.pdf')
+figure_power <- paste0(args[3], '_power_analysis.pdf')
+table_stat <- paste0(args[3], '_ttest_power_table.csv')
 
 # Load Constants and Data
-qc_pass <- c('CB2', 'H5M', '3HQ', 'PBH', 'LNA', 'YXL', 'VVN', 'R7K', 'RIW', 'TRM', 'UNY', 'W31')
+qc_pass <- c('CB2', 'H5M', '3HQ', 'PBH', 'LNA', 'YXL', 'VVN', 'R7K',
+             'RIW', 'TRM', 'UNY', 'W31')
 validation <- readr::read_tsv(validation_fh)
 protein <- read.csv(protein_fh, header = T, stringsAsFactors = F)
 
@@ -42,45 +48,22 @@ validation <- validation[validation$sample_id %in% qc_pass, ]
 validation$prediction[validation$prediction == 0] <- -1
 
 # Finalize ensemble predictions (used later for final call)
-neg_prob <- validation %>% group_by(sample_id) %>% summarise(neg_prob = sum(neg_prob))
-pos_prob <- validation %>% group_by(sample_id) %>% summarise(pos_prob = sum(pos_prob))
-pred_count <- validation %>% group_by(sample_id) %>% summarise(pred_sum = sum(prediction))
+sample_groups <- validation %>% group_by(sample_id)
+neg_prob <- sample_groups %>% summarise(neg_prob = sum(neg_prob))
+pos_prob <- sample_groups %>% summarise(pos_prob = sum(pos_prob))
+pred_count <- sample_groups %>% summarise(pred_sum = sum(prediction))
 
 # Create new column of predictions weighted by test set AUROC
 validation <- validation %>%  mutate(weighted_auc = prediction * test_auc)
-weight_auc <- validation %>% group_by(sample_id) %>% summarise(weight_auc = mean(weighted_auc))
+weight_auc <- validation %>% group_by(sample_id) %>% 
+  summarise(weight_auc = mean(weighted_auc))
 
-# Refactor for plotting
-validation$sample_id <- factor(validation$sample_id, levels = qc_pass)
-
-###################################
-# Plot Bar Chart
-###################################
-png(figure_box, height = 250, width = 360)
-ggplot(validation, aes(x = sample_id, y = weighted_auc, fill = sample_id)) +
-  geom_violin(adjust = 0.8) + xlab('') + ylab('Weighted AUROC') +
-  geom_hline(yintercept = 0, linetype="dashed", lwd = 1) +
-  theme(title = element_text(size = rel(2.2)),
-        axis.title = element_text(size = rel(0.6)),
-        axis.text.x = element_text(size = rel(1.5), angle = 45),
-        axis.text.y = element_text(size = rel(1.5)),
-        legend.position = "none",
-        panel.grid.major = element_line(color = 'white', size = 0.3),
-        panel.grid.minor = element_line(color = 'white', size = 0.3),
-        panel.background = element_rect(fill = 'white'),
-        axis.line.x = element_blank(),
-        axis.line.y = element_line(color = 'black', size = 0.5),
-        axis.ticks = element_blank())
-dev.off()
-
-###################################
-# Plot scatter plot
-###################################
+# Obtain protein by prediction correlations
 plot_ready <- cbind(pred_count, pos_prob, neg_prob, weight_auc,
-                    protein[match(pred_count$sample_id, protein$X), ])
+                    protein[match(pred_count$sample_id, protein$sample_id), ])
 plot_ready <- plot_ready[complete.cases(plot_ready), ]
-plot_ready <- plot_ready[, c('sample_id', 'pred_sum', 'pos_prob', 'neg_prob', 'weight_auc',
-                             'u87pi.norm', 'plate')]
+plot_ready <- plot_ready[, c('sample_id', 'pred_sum', 'pos_prob', 'neg_prob',
+                             'weight_auc', 'u87pi.norm', 'plate')]
 
 # Relabel to add plate info
 plot_ready[13:14, 6:7] <- protein[12:13, c(2, 4)]
@@ -92,39 +75,61 @@ h5m_mean <- mean(plot_ready[plot_ready$sample_id == 'H5M', "u87pi.norm"])
 cb2_mean <- mean(plot_ready[plot_ready$sample_id == 'CB2', "u87pi.norm"])
 
 # Subset data to find correlations
-cor_test_ready <- plot_ready[1:(nrow(plot_ready) - 2), ]
-cor_test_ready[cor_test_ready$sample_id == 'H5M', "u87pi.norm"] <- h5m_mean
-cor_test_ready[cor_test_ready$sample_id == 'CB2', "u87pi.norm"] <- cb2_mean
+protein_values <- plot_ready[1:(nrow(plot_ready) - 2), ]
+protein_values[protein_values$sample_id == 'H5M', "u87pi.norm"] <- h5m_mean
+protein_values[protein_values$sample_id == 'CB2', "u87pi.norm"] <- cb2_mean
+protein_values <- protein_values %>%
+  mutate(protein_rank = dense_rank(desc(u87pi.norm)))
 
-# Spearman correlation
-results <- cor.test(x = cor_test_ready$weight_auc, y = cor_test_ready$u87pi.norm,
-                    method = 'spearman')
+# Refactor and match protein concentration for plotting
+validation$sample_id <- factor(validation$sample_id, levels = qc_pass)
+validation <- validation %>% rowwise() %>% 
+  mutate(protein = protein_values[protein_values$sample_id == sample_id, 
+                                  'protein_rank'])
 
-# Plot
-add_text <- paste0('rho = ', round(results$estimate, 2) , '\np = ', round(results$p.value, 2))
-png(figure_scatter, height = 250, width = 360)
-ggplot(plot_ready, aes(x = weight_auc, y = u87pi.norm, color = Plate, label = sample_id)) +
-  xlab('Weighted Predictions (Mean)') + ylab('NF1 Protein Level') +
-  geom_point() + geom_text(color = 'black', vjust = -0.3, size = rel(3)) +
-  theme(title = element_text(size = rel(2)),
-        axis.title = element_text(size = rel(0.6)),
-        axis.text.x = element_text(size = rel(1.5)),
-        axis.text.y = element_text(size = rel(1.5)),
-        legend.title = element_text(size = rel(0.6)),
-        legend.key = element_rect(fill = 'white'),
-        legend.text = element_text(size = rel(1)),
+# Plot Paddle Chart
+paddle_grob <- ggplot(validation, aes(x = sample_id, y = weighted_auc,
+                                      fill = protein)) +
+  geom_violin(adjust = 0.8) +
+  xlab('') + ylab('Weighted Prediction Scores') +
+  geom_hline(yintercept = 0, linetype = "dashed", lwd = 0.5) +
+  #scale_fill_brewer(palette = 'Greys') +
+  scale_fill_distiller(palette = "Blues") +
+  theme(title = element_text(size = rel(2.2)),
+        axis.title = element_text(size = rel(0.4)),
+        axis.text.x = element_text(size = rel(1), angle = 45),
+        axis.text.y = element_text(size = rel(1)),
+        legend.position = "none",
         panel.grid.major = element_line(color = 'white', size = 0.3),
         panel.grid.minor = element_line(color = 'white', size = 0.3),
         panel.background = element_rect(fill = 'white'),
+        axis.line.x = element_blank(),
+        axis.line.y = element_line(color = 'black', size = 0.5),
+        axis.ticks = element_blank())
+
+# Plot scatter plot
+scatter_grob <- ggplot(plot_ready, aes(x = weight_auc, y = u87pi.norm,
+                                       color = Plate, label = sample_id)) +
+  xlab('Weighted Predictions (Mean)') +
+  ylab('NF1 Protein Concentration') +
+  geom_point(size = rel(0.8)) +
+  geom_text(color = 'black', hjust = 1.2, size = rel(2.2)) +
+  scale_x_continuous(breaks = seq(-1, 1, by = 0.5), limits = c(-1, 1)) +
+  theme(title = element_text(size = rel(2.2)),
+        axis.title = element_text(size = rel(0.4)),
+        axis.text.x = element_text(size = rel(1)),
+        axis.text.y = element_text(size = rel(1)),
         axis.line.x = element_line(color = 'black', size = 0.5),
         axis.line.y = element_line(color = 'black', size = 0.5),
-        axis.ticks = element_blank(),
-        title = element_text(size = 25))
-dev.off()
+        legend.title = element_text(size = rel(0.5)),
+        legend.key = element_rect(fill = 'white'),
+        legend.text = element_text(size = rel(0.6)),
+        legend.margin = unit(0.05, "cm"), 
+        panel.grid.major = element_line(color = 'white', size = 0.3),
+        panel.grid.minor = element_line(color = 'white', size = 0.3),
+        panel.background = element_rect(fill = 'white'))
 
-###################################
 # Plot barchart for positive and negative predictions
-###################################
 predict_nf1 <- vector(length = nrow(plot_ready))
 predict_nf1[plot_ready$weight_auc > 0] <- 'NF1 Inactive'
 predict_nf1[plot_ready$weight_auc <= 0] <- 'NF1 Wildtype'
@@ -143,23 +148,63 @@ predict_plot[predict_plot$sample_id == 'CB2', 'u87pi.norm'] <- cb2_mean
 
 # Set seed for geom_jitter()
 set.seed(1234)
-
-png(figure_pred, height = 250, width = 360)
-ggplot(predict_plot, aes(x = predict_nf1, y = u87pi.norm)) +
-  geom_boxplot(aes(fill = predict_nf1), outlier.colour = 'white') +
-  xlab('') + ylab('NF1 Protein Level') +
-  geom_jitter(width = 0.2) +
+box_plot_grob <- ggplot(predict_plot, aes(x = predict_nf1, y = u87pi.norm)) +
+  geom_boxplot(aes(fill = predict_nf1), lwd = 0.2, outlier.colour = 'white') +
+  xlab('') + ylab('NF1 Protein Concentration') +
+  geom_jitter(width = 0.4, size = rel(0.8)) +
   scale_fill_manual(values = c('lightblue', 'red')) +
-  theme(title = element_text(size = 20),
-        axis.title = element_text(size = rel(0.6)),
-        axis.text.x = element_text(size = rel(1.5)),
-        axis.text.y = element_text(size = rel(1.5)),
+  theme(title = element_text(size = rel(2.2)),
+        axis.title = element_text(size = rel(0.4)),
+        axis.text.x = element_text(size = rel(1)),
+        axis.text.y = element_text(size = rel(1)),
         legend.position = "none",
         panel.grid.major = element_line(color = 'white', size = 0.3),
         panel.grid.minor = element_line(color = 'white', size = 0.3),
         panel.background = element_rect(fill = 'white'),
         axis.line.x = element_line(color = 'black', size = 0.5),
         axis.line.y = element_line(color = 'black', size = 0.5),
-        axis.ticks = element_blank(),
-        title = element_text(size = 25))
+        axis.ticks.x = element_blank())
+
+# Arrage all grobs and save figure
+blankGrob <- ggplot() + geom_blank() + theme(panel.background = element_blank())
+layout <- matrix(c(1,2,3,4), nrow = 2, ncol = 2)
+pdf(figure_main, height = 5.5, width = 6.5)
+grid.arrange(blankGrob, paddle_grob, scatter_grob, box_plot_grob,
+             layout_matrix = layout)
+dev.off()
+
+# t-test for difference in protein between groups
+nf1_inactive <- predict_plot[predict_plot$predict_nf1 == 'NF1 Inactive',
+                             'u87pi.norm']
+nf1_wildtype <- predict_plot[predict_plot$predict_nf1 == 'NF1 Wildtype',
+                             'u87pi.norm']
+results <- t.test(x = nf1_inactive, y = nf1_wildtype, alternative = 'less')
+
+# Store results in table
+t_stat <- results$statistic
+p_val <- results$p.value
+effect_size <- cohensD(nf1_inactive, nf1_wildtype)
+required_n <- round(pwr.t.test(d = effect_size, power = 0.8 ,
+                               type = 'one.sample')$n)
+full_results <- cbind(t_stat, p_val, effect_size, required_n)
+write.table(full_results, table_stat, sep = ',', row.names = F)
+
+# Power analysis figure
+n <- seq(2, 100)
+power <- sapply(n, function(x) pwr.t.test(d = effect_size, n = x,
+                                          type = 'one.sample')$power)
+power_plot <- data.frame(cbind(n, power))
+pdf(figure_power, height = 3, width = 3)
+ggplot(data = power_plot, aes(x = n, y = power)) + geom_line(lwd = 0.5) +
+  theme(axis.title = element_text(size = rel(1.0)),
+        axis.text.x = element_text(size = rel(0.8)),
+        axis.text.y = element_text(size = rel(0.8)),
+        legend.position = "none",
+        panel.grid.major = element_line(color = 'white', size = 0.3),
+        panel.grid.minor = element_line(color = 'white', size = 0.3),
+        panel.background = element_rect(fill = 'white'),
+        axis.line.x = element_line(color = 'black', size = 0.5),
+        axis.line.y = element_line(color = 'black', size = 0.5)) +
+  geom_hline(yintercept = 0.80, linetype = "dashed", lwd = 0.5, col = 'red') +
+  geom_vline(xintercept = 12, linetype = 'dashed', lwd = 0.5, col = 'blue')
 dev.off()
